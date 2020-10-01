5d57ee38b0bfdd850ac349f19359d80c84bc26dc
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-10-01 21:46:29 +0530

    Add files via upload

---
+++ LambExp.java
@@ -0,0 +1,17 @@
+@FunctionalInterface
+interface MyFunctionalInterface {
+
+	//A method with single parameter
+    public int incrementByFive(int a);
+}
+public class LambExp {
+
+   public static void main(String args[]) {
+        // lambda expression with single parameter num
+    	MyFunctionalInterface f = (num) -> num+5;
+        System.out.println(f.incrementByFive(22));
+    }
+}
+
+
+
